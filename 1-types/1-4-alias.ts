77bff48f9da63d7435bdeff6d612958408ceef8e
laurenCho9 <lauren.choux@gmail.com>
2024-07-26 20:09:33 +0900

    docs: 타입스크립트의 꽃 Type Alias - Type Alias 방식에 대한 설명글 추가

--- 1-types/1-4-alias.ts
+++ 1-types/1-4-alias.ts
@@ -1,28 +1,28 @@
 {
   /**
-   * Type Aliases
+   * Type Aliases - 타입스크립트의 꽃🌹, 직접 타입의 이름을 지정해주는 방식이다.
    */
   type Text = string;
-  const name: Text = 'ellie';
-  const address: Text = 'korea';
+  const name: Text = "ellie";
+  const address: Text = "korea";
   type Num = number;
   type Student = {
     name: string;
     age: number;
   };
   const student: Student = {
-    name: 'ellie',
+    name: "ellie",
     age: 12,
   };
 
   /**
    * String Literal Types
    */
-  type Name = 'name';
+  type Name = "name";
   let ellieName: Name;
-  ellieName = 'name';
-  type JSON = 'json';
-  const json: JSON = 'json';
+  ellieName = "name";
+  type JSON = "json";
+  const json: JSON = "json";
 
   type Boal = true;
 }
