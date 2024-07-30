fc2426bb3098773d4107f95d4b70f9342f2f8542
laurenCho9 <lauren.choux@gmail.com>
2024-07-30 21:33:24 +0900

    docs: 진정한 타입스크립트의 시작! 필수 타입! - Union,  Discriminated Union 타입의 설명글 추가

--- 1-types/1-5-union.ts
+++ 1-types/1-5-union.ts
@@ -1,12 +1,12 @@
 {
   /**
-   *  Union Types: OR
+   *  Union Types: OR - 발생할 수 있는 모든 케이스 중의 하나만 할당할 수 있을 때 쓰면 좋다. (활용도 높은 타입)
    */
-  type Direction = 'left' | 'right' | 'up' | 'down';
+  type Direction = "left" | "right" | "up" | "down";
   function move(direction: Direction) {
     console.log(direction);
   }
-  move('down');
+  move("down");
 
   type TileSize = 8 | 16 | 32;
   const tile: TileSize = 16;
@@ -22,10 +22,11 @@
   };
   type LoginState = SuccessState | FailState;
 
+  // function login(id: string, password: string): Promise<LoginState> { // 네트워크 통신을 하는 비동기도 해야하니까 원래는 이런식으로 해야 함
   function login(): LoginState {
     return {
       response: {
-        body: 'logged in!',
+        body: "logged in!",
       },
     };
   }
@@ -33,8 +34,10 @@
   // printLoginState(state: LoginState)
   // success -> 🎉 body
   // fail -> 😭 reason
+  // function printLoginState(state: LoginState):void(:void는 생략가능) {
   function printLoginState(state: LoginState) {
-    if ('response' in state) {
+    // 조건: response 라는 key가 state 안에 있다면 (그러나 좋은 방법이 아녜요 1-6 Discriminated Union 방식이 좋다)
+    if ("response" in state) {
       console.log(`🎉 ${state.response.body}`);
     } else {
       console.log(`😭 ${state.reason}`);
--- 1-types/1-6-discriminated.ts
+++ 1-types/1-6-discriminated.ts
@@ -1,22 +1,22 @@
 {
   // function: login -> success, fail ⏱
   type SuccessState = {
-    result: 'success';
+    result: "success"; // 이게 추가된 것
     response: {
       body: string;
     };
   };
   type FailState = {
-    result: 'fail';
+    result: "fail"; // 이게 추가된 것
     reason: string;
   };
   type LoginState = SuccessState | FailState;
 
   function login(): LoginState {
     return {
-      result: 'success',
+      result: "success", // 이게 추가된 것
       response: {
-        body: 'logged in!',
+        body: "logged in!",
       },
     };
   }
@@ -25,10 +25,12 @@
   // success -> 🎉 body
   // fail -> 😭 reason
   function printLoginState(state: LoginState) {
-    if (state.result === 'success') {
+    if (state.result === "success") {
       console.log(`🎉 ${state.response.body}`);
     } else {
       console.log(`😭 ${state.reason}`);
     }
   }
 }
+
+// 이 디스크리미네이티드 유니온 타입을 통해 좀 더 직관적인 코드 작성이 가능합니다.
