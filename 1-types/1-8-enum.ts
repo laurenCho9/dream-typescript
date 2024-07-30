90c98219de9320d06e2008cdb7534070e35164d9
laurenCho9 <lauren.choux@gmail.com>
2024-07-30 22:30:06 +0900

    docs: enum은 무엇이고 좋은건가? - Enum 타입의 설명글 추가
    - 다른 언어에서는 유용하나, 타입스크립트에서는 쓰지 않는 것이 좋다. 왜? 타입에 값을 재할당 해도 에러가 안뜨기 때문.
    - 타입스크립트에서는 이넘 대신 union 타입을 쓰는 게 더 좋다. 그러나 딱 한군데, 앱개발 시에는 코틀린, 스위프트 등을 js로 묶어서 클라이언트에 보내야할 때 다른언어와 소통해야할 때는 서로 이해할 수 있게 이넘을 이용해야 하지만 그게 아니라면 유니온으로 얼마든지 대체 가능하고 유니온을 쓰는 게 좋다.

--- 1-types/1-8-enum.ts
+++ 1-types/1-8-enum.ts
@@ -1,6 +1,7 @@
 {
   /**
-   * Enum
+   * Enum - 다른 언어에서는 유용하나, 타입스크립트에서는 쓰지 않는 것이 좋다. 왜? 타입에 값을 재할당 해도 에러가 안뜨기 때문
+   * 타입스크립트에서는 이넘 대신 union 타입을 쓰는 게 더 좋다. 그러나 딱 한군데, 앱개발 시에는 코틀린, 스위프트 등을 js로 묶어서 클라이언트에 보내야할 때 다른언어와 소통해야할 때는 서로 이해할 수 있게 이넘을 이용해야 하지만 그게 아니라면 유니온으로 얼마든지 대체 가능하고 유니온을 쓰는 게 좋다.
    */
   // JavaScript
   const MAX_NUM = 6;
@@ -12,7 +13,7 @@
   const dayOfToday = DAYS_ENUM.MONDAY;
 
   // TypeScript
-  type DaysOfWeek = 'Monday' | 'Tuesday' | 'Wednesday';
+  type DaysOfWeek = "Monday" | "Tuesday" | "Wednesday";
   enum Days {
     Monday,
     Tuesday,
@@ -25,9 +26,9 @@
   console.log(Days.Monday);
   let day: Days = Days.Saturday;
   day = Days.Tuesday;
-  day = 10;
+  // day = 10; // 아무런 이슈 없이 컴파일러에서 동작하는 게 문제! 강의에서는 그런데 버전업 되면서 에러 출력이 나오기는 한다.
   console.log(day);
 
-  let dayOfweek: DaysOfWeek = 'Monday';
-  dayOfweek = 'Wednesday';
+  let dayOfweek: DaysOfWeek = "Monday";
+  dayOfweek = "Wednesday";
 }
