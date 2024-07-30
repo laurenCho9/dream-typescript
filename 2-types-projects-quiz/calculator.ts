c0ed692ee9dac60681d3b2f947d4feedb8404202
laurenCho9 <lauren.choux@gmail.com>
2024-07-30 23:10:25 +0900

    feat: 계산기 함수 만들기

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,27 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+console.log(calculate("add", 1, 3)); // 4
+console.log(calculate("substract", 3, 1)); // 2
+console.log(calculate("multiply", 4, 2)); // 8
+console.log(calculate("divide", 4, 2)); // 2
+console.log(calculate("remainder", 5, 2)); // 1
+
+// command 는 위의 값들이 지정되어 있고 그 값중 하나만 받을 수 있게 해야하므로 ( 또는 | 인) union 타입 사용
+type Command = "add" | "substract" | "multiply" | "divide" | "remainder";
+function calculate(command: Command, a: number, b: number): number {
+  switch (command) {
+    case "add":
+      return a + b;
+    case "substract":
+      return a - b;
+    case "multiply":
+      return a * b;
+    case "divide":
+      return a / b;
+    case "remainder":
+      return a % b;
+    default:
+      throw Error("unknown command");
+  }
+}
