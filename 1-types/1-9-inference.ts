5e692d48c6aed66721176813dae159a58d6d43f1
laurenCho9 <lauren.choux@gmail.com>
2024-07-30 22:36:24 +0900

    docs: 타입 추론은 무엇이고, 써도 되나? - 타입 추론의 설명글 추가
    변수선언시의 값은 담긴 것이 명확하므로 생략해주어도 되지만, 타입추론 보다는 정확히 타입을 명시하는 게 좋다고 생각합니다. 또한 팀프로젝트 시 정확한 스타일가이드를 명시해서 void는 생략한다. 함수에는 반드시 타입 명시를 하는 게 좋다 등 지정하는 게 좋겠습니다.

--- 1-types/1-9-inference.ts
+++ 1-types/1-9-inference.ts
@@ -1,11 +1,11 @@
 /**
- * Type Inference
+ * Type Inference - 변수선언시의 값은 담긴 것이 명확하므로 생략해주어도 되지만, 타입추론 보다는 정확히 타입을 명시하는 게 좋다고 생각합니다. 또한 팀프로젝트 시 정확한 스타일가이드를 명시해서 void는 생략한다. 함수에는 반드시 타입 명시를 하는 게 좋다 등 지정하는 게 좋겠습니다.
  */
-let text = 'hello';
-function print(message = 'hello') {
+let text = "hello";
+function print(message = "hello") {
   console.log(message);
 }
-print('hello');
+print("hello");
 
 function add(x: number, y: number): number {
   return x + y;
