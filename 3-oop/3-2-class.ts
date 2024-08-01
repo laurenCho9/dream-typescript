31453393e037ce548491358377c556033c0da55b
laurenCho9 <lauren.choux@gmail.com>
2024-08-02 00:06:35 +0900

    rename: 매개변수명 변경

--- 3-oop/3-2-class.ts
+++ 3-oop/3-2-class.ts
@@ -8,8 +8,8 @@
     static BEANS_GRAMM_PER_SHOT: number = 7; // class level
     coffeeBeans: number = 0; // instance (object) level
 
-    constructor(coffeeBeans: number) {
-      this.coffeeBeans = coffeeBeans;
+    constructor(beans: number) {
+      this.coffeeBeans = beans;
     }
 
     static makeMachine(coffeeBeans: number): CoffeeMaker {
@@ -18,7 +18,7 @@
 
     makeCoffee(shots: number): CoffeeCup {
       if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-        throw new Error('Not enough coffee beans!');
+        throw new Error("Not enough coffee beans!");
       }
       this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
       return {
