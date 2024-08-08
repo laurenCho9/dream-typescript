8413a22eb4809dc575b5142c36930b68fbe95d65
laurenCho9 <lauren.choux@gmail.com>
2024-08-08 21:41:55 +0900

    docs: 제네릭 설명글 추가

--- 5-generic/5-3-constrains.ts
+++ 5-generic/5-3-constrains.ts
@@ -22,6 +22,7 @@ function payBad(employee: Employee): Employee {
   return employee;
 }
 
+// 제네릭이긴 제네릭인데 T 타입은 Employee를 확장한 애들만 가능해 (이런식으로 제한을 둬야 좀 더 일반화된 제네릭 사용이 가능)
 function pay<T extends Employee>(employee: T): T {
   employee.pay();
   return employee;
@@ -36,18 +37,19 @@ const ellieAfterPay = pay(ellie);
 const bobAfterPay = pay(bob);
 
 const obj = {
-  name: 'ellie',
+  name: "ellie",
   age: 20,
 };
 
 const obj2 = {
-  animal: '🐕',
+  animal: "🐕",
 };
 
-console.log(getValue(obj, 'name')); // ellie
-console.log(getValue(obj, 'age')); // 20
-console.log(getValue(obj2, 'animal')); // 🐕
+console.log(getValue(obj, "name")); // ellie
+console.log(getValue(obj, "age")); // 20
+console.log(getValue(obj2, "animal")); // 🐕
 
+// T 라는 object 타입을 받아서, 대신 K라는 것은 그 obj 안에 해당하는 키 중 하나여야 하고, 리턴되는 것은 그 obj에 있는 그 key가 가르키고 있는 value 타입이어야 한다고 명시하는 것.
 function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
   return obj[key];
 }
